93883d3ecff7a5934a02973e0eece807fe038b6e
Tommaso-Desiato <tommaso_desiato@libero.it>
2023-09-22 12:38:54 +0200

    aggiunto stelle funzionanti

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -22,12 +22,14 @@ btnMinus.addEventListener("click", decrementCounter);
 function incrementCounter() {
   counter++;
   updateCounter();
+  createStar();
 }
 
 function decrementCounter() {
   if (counter > 0) {
     counter--;
     updateCounter();
+    removeStar();
   }
 }
 
@@ -38,5 +40,30 @@ function updateCounter() {
 function createStar() {
   let star = document.createElement("span");
   star.classList.add("star");
+  star.style.backgroundImage = "url('assets/img/sparkling.png')";
+
+  let maxX = window.innerWidth - 40;
+  let maxY = window.innerHeight - 40;
+
+  let randomX = Math.random() * maxX;
+  let randomY = Math.random() * maxY;
+
+  star.style.left = randomX + "px";
+  star.style.top = randomY + "px";
+
+  setTimeout(function() {
+    star.classList.add("show");
+}, 10);
+
   starContainer.appendChild(star);
+}
+
+function removeStar() {
+  let stars = starContainer.getElementsByClassName("star");
+  if (stars.length > 0) {
+      stars[stars.length - 1].classList.add("remove");
+      setTimeout(function() {
+          starContainer.removeChild(stars[stars.length - 1]);
+      }, 500); // Dopo che l'animazione è completata (0.5 secondi), rimuovi la stella
+  }
 }
